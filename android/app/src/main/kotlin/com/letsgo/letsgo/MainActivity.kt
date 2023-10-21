1a203cce24a6d12049d27d15042415af04070d60
FrancisKOUAHO <kouahofrancis@gmail.com>
2023-10-21 18:08:20 +0200

    ok

--- android/app/src/main/kotlin/com/letsgo/letsgo/MainActivity.kt
+++
@@ -1,8 +0,0 @@
-package com.letsg0.letsgo
-
-
-import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity: FlutterFragmentActivity() {
-}
