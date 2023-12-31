099598ae9f31c59c33268b35456ea677e511cb6b
Francis KOUAHO <f.kouaho@skipperndt.com>
2024-01-01 00:12:36 +0100

    fix(front) ! : stripe live

--- lib/config/url.dart
+++ lib/config/url.dart
@@ -1,11 +1,11 @@
 import 'dart:io' show Platform;
 
-class AppUrl {
+/*class AppUrl {
   static String baseUrl = Platform.isIOS ? 'http://127.0.0.1:3333/api/v1' : 'http://10.0.2.2:3333/api/v1';
   static String baseUrlImage = Platform.isIOS ? 'http://127.0.0.1:3333/uploads' : 'http://10.0.2.2:3333/uploads';
-}
+}*/
 
-/*class AppUrl {
+class AppUrl {
   static String baseUrl = 'https://api.letsgoeurope.fr/api/v1';
   static String baseUrlImage = 'https://api.letsgoeurope.fr/uploads';
-}*/
+}
